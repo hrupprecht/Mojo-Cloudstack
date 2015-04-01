47eec9181d1126c5e332043b9e65da96065d79d5
Holger Rupprecht <holger.rupprecht@bautzen-it.de>
2015-04-01 10:27:51 +0200

    added base class and ignorefiles

---
+++ lib/Mojo/Cloudstack/Base.pm
@@ -0,0 +1,20 @@
+package Mojo::Cloudstack::Base;
+use Mojo::Base -base;
+use Mojo::Util 'monkey_patch';
+
+sub new {
+  my $class = shift;
+  my $cs_class = shift;
+  my $self = bless shift, $cs_class;
+
+  no strict 'refs';
+  foreach my $key (keys %$self){
+    *{"${cs_class}::${key}"} = sub {
+      my ($self, $value) = @_;
+      return $value ? (($self->{$key} = $value) and $self) : $self->{$key};
+    }
+  }
+  return $self;
+}
+
+1;
